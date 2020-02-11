ec46df532a71f859ae122f934f46836a428a0466
XINJO <jasoncheng940111@gmail.com>
2020-02-11 16:59:22 +0800

    add new file

--- gtn.py
+++ gtn.py
@@ -1,16 +1,20 @@
 import random
-r = random.randint(1, 100)
+s = input('請決定隨機數字範圍開始值：')
+e = input('請決定隨機數字範圍結束值：')
+s = int(s)
+e = int(e)
+r = random.randint(s, e)
 x = 0
 while True :
-	num = input('請輸入數字：')
+	num = input('請猜數字：')
 	num = int(num)
 	if num == r :
 		print('恭喜猜對！答案為：',r,' 共花了',x,'次')
 		x = x + 1
 		break
-	elif num != r and num > r:
+	elif num > r:
 		print('猜錯囉，比答案大')
 		x = x + 1
-	elif num != r and num < r:
+	elif num < r:
 		print('猜錯囉，比答案小')
 		x = x + 1
