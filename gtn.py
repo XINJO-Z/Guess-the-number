54891e8e3b42c94daf99c1e300290037f4b352a0
XINJO <jasoncheng940111@gmail.com>
2020-02-11 16:44:47 +0800

    add times

--- gtn.py
+++ gtn.py
@@ -1,12 +1,16 @@
 import random
 r = random.randint(1, 100)
+x = 0
 while True :
 	num = input('請輸入數字：')
 	num = int(num)
 	if num == r :
-		print('恭喜猜對！')
+		print('恭喜猜對！答案為：',r,' 共花了',x,'次')
+		x = x + 1
 		break
 	elif num != r and num > r:
 		print('猜錯囉，比答案大')
+		x = x + 1
 	elif num != r and num < r:
 		print('猜錯囉，比答案小')
+		x = x + 1
